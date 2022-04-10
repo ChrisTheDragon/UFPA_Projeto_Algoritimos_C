33193590092695b03c964e4576d2657698378ccb
ChrisTheDragon <christiandejesus23@gmail.com>
2022-04-10 14:17:15 -0300

    soma em vetores adicionado

---
+++ Soma de dois valores de vetor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+    int vet[12], x=13, y=13;
+
+    for (int i=0; i<12; i++){
+        printf("Digite o %dº valor: ", i+1);
+        scanf("%d", &vet[i]);
+    }
+
+    printf("Numeros / Posição");
+    for (int i=0; i<12; i++){
+        printf("[%d / %d]", vet[i], i);
+    }
+    
+    while (x > 12 || y > 12)
+    {
+        printf("\nDigite uma posição do vetor: ");
+        scanf("%d", &x);
+        printf("Digite outra posição do vetor: ");
+        scanf("%d", &y);
+        if (x > 12 || y > 12) {
+            printf("Valor inválido! Digite novamente!\n");
+        }
+    }
+    
+    printf("Soma: %d + %d = %d", vet[x], vet[y], vet[x]+vet[y]);
+
+
+    return 0;
+}
